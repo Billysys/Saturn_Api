865b9bb937cac4210bce4da30d147964b92b5784
Billysys <billylav12324@gmail.com>
2025-03-09 10:05:00 +0100

    Update Class1.cs

--- SaturnApi/Class1.cs
+++ SaturnApi/Class1.cs
@@ -12,16 +12,16 @@ namespace SaturnApi
     {
         private static bool _isInjected = false;
 
-        [DllImport("dll\\Nezur.dll", CallingConvention = CallingConvention.Cdecl)]
+        [DllImport("dll\\Cloudy.dll", CallingConvention = CallingConvention.Cdecl)]
         private static extern void Initialize();
 
-        [DllImport("dll\\Nezur.dll", CallingConvention = CallingConvention.Cdecl)]
+        [DllImport("dll\\Cloudy.dll", CallingConvention = CallingConvention.Cdecl)]
         private static extern void Attach();
 
-        [DllImport("dll\\Nezur.dll", CallingConvention = CallingConvention.Cdecl)]
+        [DllImport("dll\\Cloudy.dll", CallingConvention = CallingConvention.Cdecl)]
         private static extern IntPtr GetClients();
 
-        [DllImport("dll\\Nezur.dll", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+        [DllImport("dll\\Cloudy.dll", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
         private static extern void Execute(byte[] scriptSource, string[] clientUsers, int numUsers);
 
         public static void Inject()
@@ -29,10 +29,6 @@ namespace SaturnApi
             Api.Initialize();
             _isInjected = true;
             Thread.Sleep(1000);
-            string s = "game:GetService(\"StarterGui\"):SetCore(\"SendNotification\",{\r\n\tTitle = \"Saturn X\",\r\n\tText = \"Saturn X has been injected!\",\r\n})";
-            string[] array = (from c in Api.GetClientsList()
-                              select c.name).ToArray<string>();
-            Api.Execute(Encoding.UTF8.GetBytes(s), array, array.Length);
         }
         public static bool IsInjected()
         {
